b8000317395b7e751ecba1deade531e01e1d1cab
Your Name <gjkoch7@gmail.com>
2025-02-13 23:50:41 -0300

    Work on cards products

--- src/components/newProducts/NewProducts.tsx
+++ src/components/newProducts/NewProducts.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 
 
@@ -6,42 +7,42 @@ const NewProducts = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <div className="grid w-full h-full grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-10 pt-16 pb-16 p-2 lg:p-10 drop-shadow-md">
-        <div className="relative hover:bg bg-white">
+        <Link href="/hilos" className="relative hover:bg bg-white">
           <Image
             src={"/images/bg/bg2.jpg"}
             alt={"Work1"}
             width={3000}
             height={3000}
-            className="h-full w-full object-cover rounded-md"
+            className="h-full w-full object-cover rounded-md transition-all duration-300 transform hover:shadow-2xl"
           />
           <div className="absolute bottom-10 left-10 bg-transparent text-pink-100 py-1 text-2xl border-b-4 border-pink-100">
-            <a href="/hilos">HILOS</a>
+            <span>HILOS</span>
           </div>
-        </div>
-        <div className="relative hover:bg bg-white">
+        </Link>
+        <Link href="/lanas" className="relative hover:bg bg-white">
           <Image
             src={"/images/bg/bg1.jpg"}
             alt={"Work1"}
             width={3000}
             height={3000}
-            className="h-full w-auto object-cover rounded-md"
+            className="h-full w-auto object-cover rounded-md transition-all duration-300 transform hover:shadow-2xl"
           />
           <div className="absolute bottom-10 left-10 bg-transparent text-pink-100 py-1 text-2xl border-b-4 border-pink-100">
-            <a href="/lanas">LANAS</a>
+            <span>LANAS</span>
           </div>
-        </div>
-        <div className="relative hover:bg bg-white">
+        </Link>
+        <Link href="/trabajos" className="relative hover:bg bg-white">
           <Image
             src={"/images/bg/bg3.jpg"}
             alt={"Work1"}
             width={3000}
             height={3000}
-            className="h-full w-auto object-cover rounded-md"
+            className="h-full w-auto object-cover rounded-md transition-all duration-300 transform hover:shadow-2xl"
           />
           <div className="absolute bottom-10 left-10 bg-transparent text-pink-100 py-1 text-2xl border-b-4 border-pink-100">
-            <a href="/trabajos">TRABAJOS</a>
+            <span>TRABAJOS</span>
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   )
