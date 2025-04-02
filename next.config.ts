70eae811abb4295a016724b027cf6ca92fa9207e
Your Name <gjkoch7@gmail.com>
2025-04-02 14:53:38 -0300

    Refactoring nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "marielanas.com",
+          },
+        ],
+        destination: "https://www.marielanas.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
